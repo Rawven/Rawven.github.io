51236cea0968209aaa4398bb6d971fd0d8a88565
liu <a2506991786@qq.com>
2024-08-24 17:22:22 +0800

    feat(): dd

--- src/config.mjs
+++ src/config.mjs
@@ -1,7 +1,7 @@
 export default {
     title: "Homepage",
-    titleSuffix: " | Pacamara ☕",
-    description: "Aromatic brew, pure indulgence, exquisite satisfaction.Default description",
+    titleSuffix: " Rawven",
+    description: "Never mind",
     defaultOgImage: "./defaultog.jpg",
     latestPosts: 3,
     postsPerPage: 3,
@@ -9,12 +9,12 @@ export default {
     social: {
         'dribbble': false,
         'facebook': false,
-        'github': 'https://github.com/palmiak',
+        'github': 'https://github.com/rawven',
         'instagram': false,
         'mastodon': false,
         'pinterest': false,
         'skype': false,
-        'twitter': 'https://twitter.com/palmiak_fp'
+        'twitter': ''
 
     }
 }
